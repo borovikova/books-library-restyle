a917171ed2548c260ce4c927d0c5118c75a34f22
ekaterina borovikova <borovikova@zoho.eu>
2024-10-21 09:46:35 +0200

    Split books by two columns.

--- render_website.py
+++ render_website.py
@@ -1,7 +1,9 @@
 import json
+from pprint import pprint
 
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 from livereload import Server
+from more_itertools import chunked
 
 
 def on_reload() -> None:
@@ -15,8 +17,11 @@ def on_reload() -> None:
     with open("books.json", "r") as my_file:
         books_json = my_file.read()
 
+    books = list(
+        chunked(json.loads(books_json), 2)
+    )
     rendered_page = template.render(
-        books=json.loads(books_json)
+        books=books
     )
 
     with open('index.html', 'w', encoding="utf8") as file:
